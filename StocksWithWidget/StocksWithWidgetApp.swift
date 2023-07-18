49fbc4ae757bab46caaed76fef3ce58eb1537dd8
Yauheni Levin <yauhenilevin90@gmail.com>
2023-07-18 21:42:54 +0200

    Initial commit

---
+++ StocksWithWidget/StocksWithWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksWithWidgetApp.swift
+//  StocksWithWidget
+//
+//  Created by YAUHENI LEVIN on 18.07.23.
+//
+
+import SwiftUI
+
+@main
+struct StocksWithWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
